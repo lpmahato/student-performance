7ef814fd7a67d26dac7416c885ee5b44f18a5f59
Lalu Mahato <lpmahato@msystechnologies.com>
2023-04-26 19:43:08 +0530

    Model Trained

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ import dill
 from src.logger import logging
 from src.exception import CustomException
 
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -16,5 +18,22 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        reports = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]            
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            test_score = r2_score(y_test, y_test_pred)
+
+            reports[list(models.keys())[i]] = test_score
+
+        return reports
     except Exception as e:
         raise CustomException(e, sys)
